891cb4a2021885689a2ec74c05dbc12ec31da42c
Vitor <email.vitors@gmail.com>
2022-11-28 08:34:01 -0300

    mudando o css "quem somos"

---
+++ src/pages/Robocode/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- pagina "o que é o robocode ?" -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="./style.css">
+    <title>O que é Robocode ?</title>
+</head>
+<body>
+    <?php include_once("../../components/header.php") ?>
+    <h2>O que é Robocode ?</h2>
+</body>
+</html>
