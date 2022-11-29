66fb4fe41523ab6f9e512fab71cdd8287b870146
alvarosoaress <alvarosoares01@gmail.com>
2022-11-29 10:18:35 -0300

    vercel fix #16

--- api/index.php
+++ api/index.php
@@ -1,5 +1,4 @@
-<?php echo '<script type="text/javascript">
-           window.location = "../src/pages/Home/index.php"
-      </script>';
- die();
+<?php
+ header("Location: '../src/pages/Home/index.php'");
+die();
 //require __DIR__ . '/../src/pages/Home/index.php';
