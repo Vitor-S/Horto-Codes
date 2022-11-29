a1402166551fcc989c2d986f60a4e16afe442f18
alvarosoaress <alvarosoares01@gmail.com>
2022-11-29 10:11:44 -0300

    vercel fix #13

--- api/index.php
+++ api/index.php
@@ -1,1 +1,5 @@
-<?php require __DIR__ . '/../src/pages/Home/index.php';
+<?php echo '<script type="text/javascript">
+           window.location = "http://www.google.com/"
+      </script>';
+ 
+//require __DIR__ . '/../src/pages/Home/index.php';
--- src/pages/Home/index.php
+++ src/pages/Home/index.php
@@ -4,23 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="#">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="script.js" defer></script>
 
     <title>Home</title>
 </head>
 
 <body>
-    <?php include_once("./../../components/header.php") ?>
+    <?php include_once("../../components/header.php") ?>
     <main>
         <div id="wrapper">
             <div id="carousel">
                 <div id="content">
-                    <img src="./../../assets/home/carrossel/carrossel_01.png" class="item" />
-                    <img src="./../../assets/home/carrossel/carrossel_02.png" class="item" />
-                    <img src="./../../assets/home/carrossel/carrossel_03.png" class="item" />
-                    <img src="./../../assets/home/carrossel/carrossel_04.png" class="item" />
-                    <img src="./../../assets/home/carrossel/carrossel_05.png" class="item" />
+                    <img src="../../assets/home/carrossel/carrossel_01.png" class="item" />
+                    <img src="../../assets/home/carrossel/carrossel_02.png" class="item" />
+                    <img src="../../assets/home/carrossel/carrossel_03.png" class="item" />
+                    <img src="../../assets/home/carrossel/carrossel_04.png" class="item" />
+                    <img src="../../assets/home/carrossel/carrossel_05.png" class="item" />
                 </div>
             </div>
             <button id="prev">
@@ -43,7 +43,7 @@
             </p>
         </div>
     </main>
-    <?php include_once("./../../components/footer.php") ?>
+    <?php include_once("../../components/footer.php") ?>
 </body>
 
 </html>
